3eeb4c3287a752ca0871142b6731c2615c2f2731
Nevroz Arslan <newrozarslan@gmail.com>
2022-08-23 02:24:39 +0300

    using pike lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,45 +1,10 @@
-// This package provides a Lexer that functions similarly to Rob Pike's discussion
-// about lexer design in this [talk](https://www.youtube.com/watch?v=HxaD_trXwRE).
-//
-// You can define your token types by using the `lexer.TokenType` type (`int`) via
-//
-//     const (
-//             StringToken lexer.TokenType = iota
-//             IntegerToken
-//             // etc...
-//     )
-//
-// And then you define your own state functions (`lexer.StateFunc`) to handle
-// analyzing the string.
-//
-//     func StringState(l *lexer.L) lexer.StateFunc {
-//             l.Next() // eat starting "
-//             l.Ignore() // drop current value
-//             while l.Peek() != '"' {
-//                     l.Next()
-//             }
-//             l.Emit(StringToken)
-//
-//             return SomeStateFunction
-//     }
-//
-// This Lexer is meant to emit tokens in such a fashion that it can be consumed
-// by go yacc.
 package lexer
 
 import (
-	"errors"
+	"fmt"
 	"strings"
+	"unicode"
 	"unicode/utf8"
-)
-
-type StateFunc func(*L) StateFunc
-
-type TokenType int
-
-const (
-	EOFRune    rune      = -1
-	EmptyToken TokenType = 0
 )
 
 type Token struct {
@@ -47,153 +12,308 @@ type Token struct {
 	Value string
 }
 
-type L struct {
-	source          string
-	start, position int
-	startState      StateFunc
-	Err             error
-	tokens          chan Token
-	ErrorHandler    func(e string)
-	rewind          runeStack
+type TokenType int
+
+const EOF rune = 0
+
+const LEFT_CURLY_BRACKET string = "{"
+const RIGHT_CURLY_BRACKET string = "}"
+const LEFT_ROUND_BRACKET string = "("
+const RIGHT_ROUND_BRACKET string = ")"
+
+const EQUAL_SIGN string = "="
+const NEWLINE string = "\n"
+
+const (
+	LEXER_ERROR_UNEXPECTED_EOF        string = "Unexpected end of file"
+	LEXER_ERROR_MISSING_RIGHT_BRACKET string = "Missing a closing section bracket"
+)
+
+const (
+	TOKEN_ERROR TokenType = iota
+	TOKEN_EOF
+
+	TOKEN_LEFT_CURLY_BRACKET
+	TOKEN_RIGHT_CURLY_BRACKET
+	TOKEN_LEFT_ROUND_BRACKET
+	TOKEN_RIGHT_ROUND_BRACKET
+	TOKEN_NEWLINE
+	TOKEN_COMMENT
+
+	//Move related tokens. 1.c4 or 8...d5
+	TOKEN_TURN_NUMBER
+	TOKEN_DOT
+	TOKEN_TREE_DOT
+	TOKEN_RANK
+	TOKEN_FILE
+
+	TOKEN_CHECK //Qd4+
+
+	TOKEN_CAPTURE //Qxd4
+
+)
+
+type LexFn func(*Lexer) LexFn
+
+type Lexer struct {
+	Name   string
+	Input  string
+	Tokens chan Token
+	State  LexFn
+
+	Start int
+	Pos   int
+	Width int
 }
 
-// New creates a returns a lexer ready to parse the given source code.
-func New(src string, start StateFunc) *L {
-	return &L{
-		source:     src,
-		startState: start,
-		start:      0,
-		position:   0,
-		rewind:     newRuneStack(),
+/*
+l lexer function starts everything off. It determines if we are
+beginning with a key/value assignment or a section.
+*/
+func LexBegin(lexer *Lexer) LexFn {
+	lexer.SkipWhitespace()
+
+	if strings.HasPrefix(lexer.InputToEnd(), LEFT_CURLY_BRACKET) {
+		return LexLeftCurlyBracket
+	}
+	return LexReadMove
+
+}
+
+/*
+l lexer function emits a TOKEN_COMMENT then returns
+the lexer for value.
+*/
+func LexLeftCurlyBracket(lexer *Lexer) LexFn {
+	lexer.Pos += len(LEFT_CURLY_BRACKET)
+	lexer.Emit(TOKEN_LEFT_CURLY_BRACKET)
+	return LexComment
+}
+
+/*
+l lexer function emits a TOKEN_COMMENT then returns
+the lexer for value.
+*/
+func LexComment(lexer *Lexer) LexFn {
+	for {
+		if lexer.IsEOF() {
+			return lexer.Errorf(LEXER_ERROR_MISSING_RIGHT_BRACKET)
+		}
+
+		if strings.HasPrefix(lexer.InputToEnd(), RIGHT_CURLY_BRACKET) {
+			lexer.Emit(TOKEN_COMMENT)
+			return LexRightCurlyBracket
+		}
+
+		lexer.Inc()
 	}
 }
 
-// Start begins executing the Lexer in an asynchronous manner (using a goroutine).
-func (l *L) Start() {
-	// Take half the string length as a buffer size.
-	buffSize := len(l.source) / 2
-	if buffSize <= 0 {
-		buffSize = 1
+/*
+l lexer function emits a TOKEN_COMMENT then returns
+the lexer for value.
+*/
+func LexRightCurlyBracket(lexer *Lexer) LexFn {
+	lexer.Pos += len(RIGHT_CURLY_BRACKET)
+	lexer.Emit(TOKEN_RIGHT_CURLY_BRACKET)
+	return LexReadMove
+}
+
+/*
+l lexer function emits a TOKEN_COMMENT then returns
+the lexer for value.
+*/
+func LexReadMove(l *Lexer) LexFn {
+
+	for {
+
+		if unicode.IsDigit(l.Peek()) {
+			l.Emit(TOKEN_TURN_NUMBER)
+			return
+		}
+
+		l.Inc()
 	}
-	l.tokens = make(chan Token, buffSize)
-	go l.run()
 }
 
-func (l *L) StartSync() {
-	// Take half the string length as a buffer size.
-	buffSize := len(l.source) / 2
-	if buffSize <= 0 {
-		buffSize = 1
+/*
+Start a new lexer with a given input string. l returns the
+instance of the lexer and a channel of tokens. Reading l stream
+is the way to parse a given input and perform processing.
+*/
+func BeginLexing(name, input string) *Lexer {
+	l := &Lexer{
+		Name:   name,
+		Input:  input,
+		State:  LexBegin,
+		Tokens: make(chan Token, 3),
 	}
-	l.tokens = make(chan Token, buffSize)
-	l.run()
+
+	return l
 }
 
-// Current returns the value being being analyzed at this moment.
-func (l *L) Current() string {
-	return l.source[l.start:l.position]
+/*
+Backup to the beginning of the last read token.
+*/
+func (l *Lexer) Backup() {
+	l.Pos -= l.Width
 }
 
-// Emit will receive a token type and push a new token with the current analyzed
-// value into the tokens channel.
-func (l *L) Emit(t TokenType) {
-	tok := Token{
-		Type:  t,
-		Value: l.Current(),
+/*
+Returns a slice of the current input from the current lexer start position
+to the current position.
+*/
+func (l *Lexer) CurrentInput() string {
+	return l.Input[l.Start:l.Pos]
+}
+
+/*
+Decrement the position
+*/
+func (l *Lexer) Dec() {
+	l.Pos--
+}
+
+/*
+Puts a token onto the token channel. The value of l token is
+read from the input based on the current lexer position.
+*/
+func (l *Lexer) Emit(tokenType TokenType) {
+	l.Tokens <- Token{Type: tokenType, Value: l.Input[l.Start:l.Pos]}
+	l.Start = l.Pos
+}
+
+/*
+Returns a token with error information.
+*/
+func (l *Lexer) Errorf(format string, args ...interface{}) LexFn {
+	l.Tokens <- Token{
+		Type:  TOKEN_ERROR,
+		Value: fmt.Sprintf(format, args...),
 	}
-	l.tokens <- tok
-	l.start = l.position
-	l.rewind.clear()
+
+	return nil
 }
 
-// Ignore clears the rewind stack and then sets the current beginning position
-// to the current position in the source which effectively ignores the section
-// of the source being analyzed.
-func (l *L) Ignore() {
-	l.rewind.clear()
-	l.start = l.position
+/*
+Ignores the current token by setting the lexer's start
+position to the current reading position.
+*/
+func (l *Lexer) Ignore() {
+	l.Start = l.Pos
 }
 
-// Peek performs a Next operation immediately followed by a Rewind returning the
-// peeked rune.
-func (l *L) Peek() rune {
-	r := l.Next()
-	l.Rewind()
-
-	return r
+/*
+Increment the position
+*/
+func (l *Lexer) Inc() {
+	l.Pos++
+	if l.Pos >= utf8.RuneCountInString(l.Input) {
+		l.Emit(TOKEN_EOF)
+	}
 }
 
-// Rewind will take the last rune read (if any) and rewind back. Rewinds can
-// occur more than once per call to Next but you can never rewind past the
-// last point a token was emitted.
-func (l *L) Rewind() {
-	r := l.rewind.pop()
-	if r > EOFRune {
-		size := utf8.RuneLen(r)
-		l.position -= size
-		if l.position < l.start {
-			l.position = l.start
+/*
+Return a slice of the input from the current lexer position
+to the end of the input string.
+*/
+func (l *Lexer) InputToEnd() string {
+	return l.Input[l.Pos:]
+}
+
+/*
+Returns the true/false if the lexer is at the end of the
+input stream.
+*/
+func (l *Lexer) IsEOF() bool {
+	return l.Pos >= len(l.Input)
+}
+
+/*
+Returns true/false if then next character is whitespace
+*/
+func (l *Lexer) IsWhitespace() bool {
+	ch, _ := utf8.DecodeRuneInString(l.Input[l.Pos:])
+	return unicode.IsSpace(ch)
+}
+
+/*
+Reads the next rune (character) from the input stream
+and advances the lexer position.
+*/
+func (l *Lexer) Next() rune {
+	if l.Pos >= utf8.RuneCountInString(l.Input) {
+		l.Width = 0
+		return EOF
+	}
+
+	result, width := utf8.DecodeRuneInString(l.Input[l.Pos:])
+
+	l.Width = width
+	l.Pos += l.Width
+	return result
+}
+
+/*
+Return the next token from the channel
+*/
+func (l *Lexer) NextToken() Token {
+	for {
+		select {
+		case token := <-l.Tokens:
+			return token
+		default:
+			l.State = l.State(l)
 		}
 	}
+
 }
 
-// Next pulls the next rune from the Lexer and returns it, moving the position
-// forward in the source.
-func (l *L) Next() rune {
-	var (
-		r rune
-		s int
-	)
-	str := l.source[l.position:]
-	if len(str) == 0 {
-		r, s = EOFRune, 0
-	} else {
-		r, s = utf8.DecodeRuneInString(str)
-	}
-	l.position += s
-	l.rewind.push(r)
-
-	return r
+/*
+Returns the next rune in the stream, then puts the lexer
+position back. Basically reads the next rune without consuming
+it.
+*/
+func (l *Lexer) Peek() rune {
+	rune := l.Next()
+	l.Backup()
+	return rune
 }
 
-// Take receives a string containing all acceptable strings and will contine
-// over each consecutive character in the source until a token not in the given
-// string is encountered. This should be used to quickly pull token parts.
-func (l *L) Take(chars string) {
-	r := l.Next()
-	for strings.ContainsRune(chars, r) {
-		r = l.Next()
-	}
-	l.Rewind() // last next wasn't a match
-}
-
-// NextToken returns the next token from the lexer and a value to denote whether
-// or not the token is finished.
-func (l *L) NextToken() (*Token, bool) {
-	if tok, ok := <-l.tokens; ok {
-		return &tok, false
-	} else {
-		return nil, true
-	}
-}
-
-// Partial yyLexer implementation
-
-func (l *L) Error(e string) {
-	if l.ErrorHandler != nil {
-		l.Err = errors.New(e)
-		l.ErrorHandler(e)
-	} else {
-		panic(e)
-	}
-}
-
-// Private methods
-
-func (l *L) run() {
-	state := l.startState
-	for state != nil {
+/*
+Starts the lexical analysis and feeding tokens into the
+token channel.
+*/
+func (l *Lexer) Run() {
+	for state := LexBegin; state != nil; {
 		state = state(l)
 	}
-	close(l.tokens)
+
+	l.Shutdown()
+}
+
+/*
+Shuts down the token stream
+*/
+func (l *Lexer) Shutdown() {
+	close(l.Tokens)
+}
+
+/*
+Skips whitespace until we get something meaningful.
+*/
+func (l *Lexer) SkipWhitespace() {
+	for {
+		ch := l.Next()
+
+		if !unicode.IsSpace(ch) {
+			l.Dec()
+			break
+		}
+
+		if ch == EOF {
+			l.Emit(TOKEN_EOF)
+			break
+		}
+	}
 }
--- scanner.go
+++ scanner.go
@@ -6,9 +6,9 @@ import (
 	"os"
 	"strings"
 
+	"github.com/narslan/schach/pgnparser/lexer"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
-	"gopkg.in/freeeve/pgn.v1"
 )
 
 const (
@@ -18,14 +18,11 @@ const (
 	InMoves
 )
 
+type Tag [2]string
 type Game struct {
-	Tags   [][]string
+	Tags   []Tag
 	Moves  []string
 	Result string
-}
-
-type Parser struct {
-	Games []*Game
 }
 
 func main() {
@@ -33,26 +30,13 @@ func main() {
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 
 	// An artificial input source.
-	f, err := os.Open("pgn/famous_games.pgn")
-	//f, err := os.Open("data/counter-vs-zahak.pgn")
+	//f, err := os.Open("pgn/famous_games.pgn")
+	f, err := os.Open("data/counter-vs-zahak.pgn")
+	//f, err := os.Open("data/anders.pgn")
 	if err != nil {
 		panic(err)
 	}
 	defer f.Close()
-	// splitPoint := make([]int, 0)
-	// splitPoint = append(splitPoint, 0)
-	// for i, v := range scanPGNBuf(f) {
-	// 	if i%2 != 0 {
-	// 		splitPoint = append(splitPoint, v)
-	// 	}
-
-	// }
-	// f.Seek(0, 0)
-	// games := extractGames(splitPoint, f)
-
-	// //fmt.Println(games[0])
-	// g := games[0]
-	// g.ParseMoves()
 
 	scanner := bufio.NewScanner(f)
 
@@ -68,53 +52,74 @@ func main() {
 		sa = append(sa, "["+t)
 	}
 
-	for i, v := range sa {
-		fmt.Printf("[%d] %v\n", i, v)
-	}
-
-	fmt.Printf("length %d", len(sa))
-
-	//FreevePGN()
-}
-
-func FreevePGN() {
-
-	f, err := os.Open("pgn/famous_games.pgn")
-	if err != nil {
-		panic(err)
-	}
-
-	ps := pgn.NewPGNScanner(f)
-	// while there's more to read in the file
-	for ps.Next() {
-		// scan the next game
-		game, err := ps.Scan()
-		if err != nil {
-			log.Fatal().Msgf("%+v", err)
-		}
-
-		// print out tags
-		fmt.Println(game.Tags)
-
-		// make a new board so we can get FEN positions
-		b := pgn.NewBoard()
-		for _, move := range game.Moves {
-			// make the move on the board
-			b.MakeMove(move)
-			// print out FEN for each move in the game
-			fmt.Println(b)
-		}
+	g := LexGameInput(sa[0])
+	//fmt.Printf("%s", g)
+	for _, v := range g.Tags {
+		fmt.Printf("%s", v)
 	}
 }
 
-//
+func LexGameInput(src string) (g Game) {
+
+	s := bufio.NewScanner(strings.NewReader(src))
+
+	g.Tags = make([]Tag, 0)
+
+	tags := make([]Tag, 0)
+	var movesSrc strings.Builder
+	for s.Scan() {
+
+		l := s.Text()
+		if l == "" {
+			continue
+		}
+
+		if strings.Index(l, "[") == 0 {
+			// splitted tags into key and value.
+			//fmt.Printf("tag: %s", l)
+			stags := strings.Split(l[1:len(l)-1], " ")
+			k := stags[0]
+			v := strings.ReplaceAll(stags[1], "\"", "")
+			tagnames := [2]string{k, v}
+			tags = append(tags, tagnames)
+			continue
+		}
+		fmt.Fprintf(&movesSrc, "%s", l)
+	}
+	if err := s.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading input:", err)
+	}
+	parseMoves(movesSrc.String())
+	g.Tags = tags
+	return
+}
+
+func parseMoves(src string) {
+	l := lexer.BeginLexing("deneme", src)
+	for {
+		token := l.NextToken()
+
+		fmt.Printf("%+v\n", token)
+	}
+}
+
+//Tags
+
+func (t Tag) String() string {
+	var sb strings.Builder
+
+	fmt.Fprintf(&sb, "%13s", t[0])
+	sb.WriteString(" => ")
+	fmt.Fprintf(&sb, "%s", t[1])
+	sb.WriteRune('\n')
+
+	return sb.String()
+}
+
 func (g Game) String() string {
 	var sb strings.Builder
-	for _, v := range g.Tags {
-		fmt.Fprintf(&sb, "%13s", v[0])
-		sb.WriteString(" => ")
-		fmt.Fprintf(&sb, "%s", v[1])
-		sb.WriteRune('\n')
+	for _, t := range g.Tags {
+		fmt.Fprintf(&sb, "%s", t)
 	}
 
 	for i, v := range g.Moves {
@@ -123,74 +128,6 @@ func (g Game) String() string {
 	}
 	return sb.String()
 }
-
-// func scanPGNBuf(f *os.File) (cutPoints []int) {
-
-// 	scanner := bufio.NewScanner(f)
-
-// 	var lineNumber = 1
-
-// 	cutPoints = make([]int, 0)
-
-// 	for scanner.Scan() {
-// 		line := scanner.Text()
-
-// 		if line == "" {
-// 			cutPoints = append(cutPoints, lineNumber)
-// 		}
-
-// 		lineNumber++
-// 		//log.Debug().Msg(line)
-// 	}
-
-// 	if err := scanner.Err(); err != nil {
-// 		fmt.Printf("Invalid input: %s", err)
-// 	}
-// 	return cutPoints
-// }
-
-// func extractGames(splitPoints []int, f *os.File) []Game {
-
-// 	scanner := bufio.NewScanner(f)
-// 	stacks := make([]Game, 0)
-// 	//var lineNumber = 1
-
-// 	for j, ln := range splitPoints {
-// 		//log.Debug().Msgf("ln %d", ln)
-// 		var g Game
-// 		if j == len(splitPoints)-1 {
-// 			break
-// 		}
-// 		for i := ln; i < splitPoints[j+1]; i++ {
-
-// 			scanner.Scan()
-
-// 			//log.Debug().Msgf("%+v", scanner.Text())
-// 			line := scanner.Text()
-// 			if strings.HasPrefix(line, "[") && strings.HasSuffix(line, "]") {
-// 				before, after, ok := strings.Cut(line, " \"")
-// 				if ok {
-// 					s := make([]string, 0)
-// 					s = append(s, before[1:], after[:len(after)-2])
-// 					g.Tags = append(g.Tags, s)
-// 				}
-
-// 			} else {
-// 				if line != "" {
-// 					g.Moves = append(g.Moves, line)
-// 				}
-
-// 			}
-
-// 		}
-// 		stacks = append(stacks, g)
-// 	}
-
-// 	if err := scanner.Err(); err != nil {
-// 		log.Fatal().Msgf("%+v", err)
-// 	}
-// 	return stacks
-// }
 
 // IsEmpty: check if stack is empty
 func (g Game) IsEmpty() bool {
@@ -202,6 +139,7 @@ func (g Game) ParseMoves() {
 
 }
 
+// Taken from: https://github.com/freeeve/pgn/issues/17.
 func crunchSplitFunc(data []byte, atEOF bool) (advance int, token []byte, err error) {
 	if atEOF && len(data) == 0 {
 		return 0, nil, nil
